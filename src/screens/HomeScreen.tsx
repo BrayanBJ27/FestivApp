6bed32e1095187f16dad3bed08fcf61342008311
BrayanBJ27 <xavi_braian@hotmail.com>
2025-01-18 00:42:13 -0500

    feat: :sparkles: Integrate BottomNavbar component into HomeScreen for improved navigation

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -9,6 +9,7 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import BottomNavbar from "../components/BottomNavbar";
 import MainStyles from '../styles/MainStyles';
 
 const HomeScreen: React.FC = (): JSX.Element => {
@@ -85,72 +86,7 @@ const HomeScreen: React.FC = (): JSX.Element => {
               </ImageBackground>
             </View>
             {/* Footer Navigation*/}
-            <View style={MainStyles.bottomNavContainer}>
-              <TouchableOpacity 
-                style={MainStyles.navButton}
-                onPress={() => setActiveTab('Home')}
-              >
-                <Icon name="home" size={24} style={activeTab === 'Home' ? MainStyles.activeIcon : MainStyles.inactiveIcon}/>
-                <Text style={[MainStyles.navText, 
-                    activeTab === 'Home' ? MainStyles.activeNavText : MainStyles.inactiveNavText]}>
-                  Home
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity 
-                style={MainStyles.navButton}
-                onPress={() => setActiveTab('Notification')}
-              >
-                <Icon 
-                  name="bell" size={24} 
-                  style={activeTab === 'Notification' ? MainStyles.activeIcon : MainStyles.inactiveIcon}
-                />
-                <Text 
-                  style={[
-                    MainStyles.navText,
-                    activeTab === 'Notification' ? MainStyles.activeNavText : MainStyles.inactiveNavText
-                  ]}
-                >
-                  Notification
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity 
-                style={MainStyles.navButton}
-                onPress={() => setActiveTab('Map')}
-              >
-                <Icon 
-                  name="map-marker" size={24} 
-                  style={activeTab === 'Map' ? MainStyles.activeIcon : MainStyles.inactiveIcon}
-                />
-                <Text 
-                  style={[
-                    MainStyles.navText,
-                    activeTab === 'Map' ? MainStyles.activeNavText : MainStyles.inactiveNavText
-                  ]}
-                >
-                  Map
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity 
-                style={MainStyles.navButton}
-                onPress={() => setActiveTab('Account')}
-              >
-                <Icon 
-                  name="user" size={24} 
-                  style={activeTab === 'Account' ? MainStyles.activeIcon : MainStyles.inactiveIcon}
-                />
-                <Text 
-                  style={[
-                    MainStyles.navText,
-                    activeTab === 'Account' ? MainStyles.activeNavText : MainStyles.inactiveNavText
-                  ]}
-                >
-                  Account
-                </Text>
-              </TouchableOpacity>
-            </View>
+            <BottomNavbar activeTab={activeTab} setActiveTab={setActiveTab} />
           </View>
       </ScrollView>
     </SafeAreaView>
