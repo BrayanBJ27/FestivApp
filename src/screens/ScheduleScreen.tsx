c3859bf08a470ec6d1e9df1981d3385f8ab47e3b
BrayanBJ27 <xavi_braian@hotmail.com>
2025-01-23 01:23:53 -0500

    feat: Update ScheduleScreen with new icons and improved date formatting

--- src/screens/ScheduleScreen.tsx
+++ src/screens/ScheduleScreen.tsx
@@ -7,7 +7,7 @@ import {
   ScrollView,
 } from 'react-native';
 import BottomNavbar from "../components/BottomNavbar";
-import Icon from "react-native-vector-icons/FontAwesome";
+import Icon from "react-native-vector-icons/FontAwesome6";
 import MainStyles from "../styles/MainStyles";
 
 const ScheduleScreen: React.FC = (): JSX.Element => {
@@ -38,31 +38,34 @@ const ScheduleScreen: React.FC = (): JSX.Element => {
             <Icon
                 name="location-arrow"
                 size={20}
-                color="#adadad"
-                style={MainStyles.inputIcon}
+                color="#fff"
               />
-              <Text style={MainStyles.locationTextSS}>Pillaro</Text>
+              <Text style={MainStyles.locationTextSS}>Pillaro/Tungurahua</Text>
               <View style={MainStyles.buttonContainerSS}>
-                <Text style={MainStyles.buttonTextSS}>Tungurahua</Text>
+              <Icon name="calendar" size={20} color="#ffffff" />
               </View>
             </View>
           </ImageBackground>
-
           <View style={MainStyles.dragIndicatorSS} />
           <View style={MainStyles.scheduleContainerSS}>
             <View style={MainStyles.scheduleHeaderSS}>
               <Text style={MainStyles.scheduleTitleSS}>Schedule</Text>
             </View>
-
             <View style={MainStyles.scheduleItemSS}>
               <Text style={MainStyles.timeTextSS}>10:30</Text>
               <View style={MainStyles.timelineSS}>
                 <View style={MainStyles.timelineActiveSS} />
               </View>
               <Text style={MainStyles.locationNameSS}>Píllaro Central</Text>
-              <Text style={MainStyles.dateTextSS}>January 1st</Text>
+              <Text style={MainStyles.dateTextSS}>Jan 1</Text>
               <Icon
-                name="map"
+                name="cloud-rain"
+                size={20}
+                color="#0373f3"
+                style={MainStyles.weatherIconSS}
+              />
+              <Icon
+                name="location-dot"
                 size={20}
                 color="#0373f3"
                 style={MainStyles.scheduleIconSS}
@@ -75,9 +78,15 @@ const ScheduleScreen: React.FC = (): JSX.Element => {
                 <View style={MainStyles.timelineInactiveSS} />
               </View>
               <Text style={MainStyles.locationNameSS}>San Vicente de Quilimbulo</Text>
-              <Text style={MainStyles.dateTextSS}>January 2nd-3rd</Text>
+              <Text style={MainStyles.dateTextSS}>Jan 2nd-3rd</Text>
               <Icon
-                name="map"
+                name="sun"
+                size={20}
+                color="#adadad"
+                style={MainStyles.weatherIconSS}
+              />
+              <Icon
+                name="location-dot"
                 size={20}
                 color="#adadad"
                 style={MainStyles.scheduleIconSS}
@@ -85,14 +94,20 @@ const ScheduleScreen: React.FC = (): JSX.Element => {
             </View>
 
             <View style={MainStyles.scheduleItemSS}>
-              <Text style={MainStyles.timeTextSS}>8:00</Text>
+              <Text style={MainStyles.timeTextSS}>15:00</Text>
               <View style={MainStyles.timelineSS}>
                 <View style={MainStyles.timelineInactiveSS} />
               </View>
               <Text style={MainStyles.locationNameSS}>Tunguipamba</Text>
-              <Text style={MainStyles.dateTextSS}>January 4th-5th</Text>
+              <Text style={MainStyles.dateTextSS}>Jan 4th-5th</Text>
               <Icon
-                name="map"
+                name="cloud"
+                size={20}
+                color="#adadad"
+                style={MainStyles.weatherIconSS}
+              />
+              <Icon
+                name="location-dot"
                 size={20}
                 color="#adadad"
                 style={MainStyles.scheduleIconSS}
