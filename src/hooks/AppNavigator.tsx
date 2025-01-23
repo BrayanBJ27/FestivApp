886ab9277bb24b19d3049aadf9826b30eb0fb216
BrayanBJ27 <xavi_braian@hotmail.com>
2025-01-23 11:53:24 -0500

    feat: Update AppNavigator to include Login and SignUp screens

--- src/hooks/AppNavigator.tsx
+++ src/hooks/AppNavigator.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+
+import LoginScreen from "../screens/LoginScreen";
+import SingInScreen from "../screens/SingInScreen";
 import HomeScreen from "../screens/HomeScreen";
 import EventScreen from "../screens/EventScreen";
 import ScheduleScreen from "../screens/ScheduleScreen";
@@ -8,40 +11,27 @@ import MapScreen from "../screens/MapScreen";
 import AccountScreen from "../screens/AccountScreen";
 import NotificationScreen from "../screens/NotificationScreen";
 
-// Define las rutas esperadas en la navegación
-export type RootStackParamList = {
-  Home: undefined;
-  Event: undefined;
-  Schedule: undefined;
-  Map: undefined;
-  Account: undefined;
-  Notification: undefined;
-};
+import { RootStackParamList } from "../types/types";
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 const AppNavigator: React.FC = (): JSX.Element => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SingInScreen}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Map"
-          component={MapScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Notification"
-          component={NotificationScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Account"
-          component={AccountScreen}
           options={{ headerShown: false }}
         />
         <Stack.Screen
@@ -52,6 +42,21 @@ const AppNavigator: React.FC = (): JSX.Element => {
         <Stack.Screen
           name="Schedule"
           component={ScheduleScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Map"
+          component={MapScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Account"
+          component={AccountScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Notification"
+          component={NotificationScreen}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
