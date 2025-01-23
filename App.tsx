d1f4da313d70182536254be9c8d4c6b4a1163adf
BrayanBJ27 <xavi_braian@hotmail.com>
2025-01-23 11:53:02 -0500

    feat: Refactor App component to use AppNavigator and UserProvider

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { UserProvider } from "./src/hooks/UserContext"; // Importa el contexto
+import { UserProvider } from "./src/hooks/UserContext";
+import AppNavigator from "./src/hooks/AppNavigator";
 
 import LoginScreen from "./src/screens/LoginScreen";
 import SignUpScreen from "./src/screens/SingInScreen";
@@ -17,47 +18,9 @@ import NotificationScreen from "./src/screens/NotificationScreen";
 const Stack = createStackNavigator();
 
 export default function App() {
-
-    // const [fontsLoaded, setFontsLoaded] = useState(false);
-    // return (
-    //   <UserProvider>
-    //     <NavigationContainer>
-    //       <Stack.Navigator initialRouteName="Login">
-    //         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-    //         <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false }} />
-    //         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-    //       </Stack.Navigator>
-    //     </NavigationContainer>
-    //   </UserProvider>
-    // );
-
-    // useEffect(() => {
-    //   async function loadFonts() {
-    //     await Font.loadAsync({
-    //       // Nombres personalizados para las fuentes
-    //       "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
-    //       "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
-    //     });
-    //     setFontsLoaded(true);
-    //   }
-    //   loadFonts();
-    // }, []);
-  
-    // if (!fontsLoaded) {
-    //   return (
-    //     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    //       <ActivityIndicator size="large" />
-    //     </View>
-    //   );
-    // }
-    // return <LoginScreen />;
-    // return <SingInScreen />;
-    // return <HomeScreen />;
-    // return <EventScreen />;
-    // return <MapScreen />;
-    // return <ScheduleScreen />;
-    // return <CalendarScreen />;
-    // return <HistoryScreen />;
-    // return <AccountScreen />;
-    return <NotificationScreen />;
-}
+    return (
+        <UserProvider>
+          <AppNavigator />
+        </UserProvider>
+      );
+    }
