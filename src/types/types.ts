98f306bb0feb877263ae220a82392b0c375cf214
BrayanBJ27 <xavi_braian@hotmail.com>
2025-01-30 14:43:39 -0500

    feat: Add AdminDashboard route to RootStackParamList

--- src/types/types.ts
+++ src/types/types.ts
@@ -12,4 +12,5 @@ export type RootStackParamList = {
   History: undefined;
   TermsScreen: undefined; // Agrega la ruta para TermsScreen
   HelpCenterScreen: undefined
+  AdminDashboard: undefined;
 };
