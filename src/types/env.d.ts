40e0432352723c2dab875c853129af188fee4d97
BrayanBJ27 <xavi_braian@hotmail.com>
2025-01-30 14:43:44 -0500

    feat: Add Firebase API key and project ID to environment type definitions

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,5 +1,7 @@
 declare module "@env" {
     export const GOOGLE_PLACES_API_KEY: string;
     export const WEATHER_API_KEY: string;
+    export const EXPO_PUBLIC_FIREBASE_API_KEY: string;
+    export const EXPO_PUBLIC_FIREBASE_PROJECT_ID: string;
   }
   
